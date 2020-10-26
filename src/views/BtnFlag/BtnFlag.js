0062d1bd749e39faeb336494bcac1ecfb7719174
lingweihao <1020529941@qq.com>
2020-10-27 00:10:35 +0800

    fix:003 jsx 注释 一半

--- src/views/BtnFlag/BtnFlag.js
+++ src/views/BtnFlag/BtnFlag.js
@@ -4,21 +4,34 @@ import './BtnFlag.css';
 import ReactDOM from 'react-dom';
 
 let random = Math.round(Math.random())
-
-let className = random?"redBgm":"blueBgm"
+let borderBottom = {
+    borderBottom: "1px solid #000", // 首字母大写
+    "border-top":"1px solid #000" // 字符串
+}
+let className = random ? "redBgm" : "blueBgm"
 let element = (<div >
     <div>
         03 jsx 和if判断
     </div>
     <div className={className} >
-        当前的颜色：{random?"红色":"蓝色"}
+        当前的颜色：{random ? "红色" : "蓝色"}
     </div>
 </div>)
+// css 
+let element2 = (
 
-
-export default function() {
+<div >
+    {/*在这里注释 */}
+    <div>
+        03 jsx 和if判断
+    </div>
+    <div className={'bigTxt ' + className} style={borderBottom} >
+        当前的颜色：{random ? "红色" : "蓝色"}
+    </div>
+</div>)
+export default function () {
     ReactDOM.render(
-        element,
+        element2,
         document.querySelector('#example03')
     );
 }
